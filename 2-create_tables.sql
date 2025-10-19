eae6072b0376263299b426250f0c0475a305e626
de-scientist <gitaumark502@gmail.com>
2025-10-19 15:16:07 +0300

    verify the current database

---
+++ 2-create_tables.sql
@@ -0,0 +1,32 @@
+--create tables for database school
+USE School;
+
+--verify the current database
+SELECT DB_NAME() AS CurrentDatabase;
+
+CREATE TABLE Departments (
+	DepartmentID NVARCHAR(20) PRIMARY KEY,
+	DepartmentTitle NVARCHAR(100) NOT NULL
+);
+
+
+CREATE TABLE Teachers (
+	Id  NVARCHAR(20) PRIMARY KEY,
+	FirstName  NVARCHAR(50) NOT NULL,
+	LastName NVARCHAR(50) NOT NULL,
+	EmailAddress NVARCHAR(100) NOT NULL UNIQUE,
+	PhoneNumber  NVARCHAR(15) NOT NULL UNIQUE,
+	DepartmentID  NVARCHAR(20) FOREIGN KEY REFERENCES DEPARTMENTS(DepartmentID)
+);
+
+CREATE TABLE Majors (
+	MajorID  NVARCHAR(20) PRIMARY KEY,
+	MajorTitle NVARCHAR(100) NOT NULL
+);
+
+CREATE TABLE Students (
+	StudentID NVARCHAR(20) PRIMARY KEY,
+	FirstName NVARCHAR(50) NOT NULL,
+	LastName  NVARCHAR(50) NOT NULL,
+	MajorID  NVARCHAR(20) NOT NULL FOREIGN KEY REFERENCES Majors(MajorID)
+);
